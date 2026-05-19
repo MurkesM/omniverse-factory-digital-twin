6298839400bff9ab357f4d50bdc41d08572eebf9
MurkesM <91104156+MurkesM@users.noreply.github.com>
2026-05-18 20:45:05 -0500

    created initial kit file

--- premake5.lua
+++ premake5.lua
@@ -15,3 +15,5 @@ repo_build.prebuild_copy {
 }
 
 -- Apps: for each app generate batch files and a project based on kit files (e.g. my_name.my_app.kit)
+
+define_app("ethan.omniverse_factory_digital_twin.kit")
